827890708f0c92ab7f08543721ad423450db96e6
David Nzube <davidnzube2007@outlook.com>
2024-01-15 22:37:04 +0100

    Installed React

--- index.js
+++ index.js
@@ -114,3 +114,16 @@ app.listen(PORT, () => {
 // }
 
 // export default App;
+
+
+// Bluvid@Madubuike MINGW64 /c/ReaGistery (master)
+// $ npx create-react-app reagistery
+// npm ERR! code ENOENT
+// npm ERR! syscall lstat
+// npm ERR! path C:\Users\Bluvid\AppData\Roaming\npm
+// npm ERR! errno -4058
+// npm ERR! enoent ENOENT: no such file or directory, lstat 'C:\Users\Bluvid\AppData\Roaming\npm'
+// npm ERR! enoent This is related to npm not being able to find a file.
+// npm ERR! enoent
+
+// npm ERR! A complete log of this run can be found in: C:\Users\Bluvid\AppData\Local\npm-cache\_logs\2024-01-15T19_41_13_865Z-debug-0.log
